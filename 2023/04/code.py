8cbb1c5a0817d4b5bf04ef55079ff0c75cb55e07
Regdon <marks83@hotmail.co.uk>
2024-01-07 12:08:39 +0000

    Part 2 solved

--- 2023/04/code.py
+++ 2023/04/code.py
@@ -1,12 +1,13 @@
 #Advent of Code 2023 Day 4
 #Part 1: For each card how many numbers are on the left and right hand side. Calculate 2^n-1 for each card. Add up result.
-#Part 2:
+#Part 2: With n winning numbers on a card, you get 1 copy of the next n cards. If you have n copies of a card, that will mean n copes of the next cards for a win. How many total cards?
 
 class card:
     def __init__(self, data):
         self.cardNumber = 0
         self.winningNumbers = set()
         self.ourNumbers = set()
+        self.copies = 1
 
         data = data.split(':')
         self.cardNumber = int(data[0].replace('Card', ''))
@@ -31,6 +32,10 @@ class card:
             return 0
         else:
             return pow(2, len(wins) - 1)
+        
+    def countWinning(self):
+        wins = self.winningNumbers.intersection(self.ourNumbers)
+        return len(wins)
 
 
 cards = []
@@ -44,3 +49,16 @@ with open('2023/04/input.txt') as f:
         totalPoints += currentCard.points()
 
     print('Part 1: Toatal Points =', totalPoints)
+
+id = 0
+totalCards = 0
+for card in cards:
+    currentCardWins = card.countWinning()
+    while currentCardWins > 0:
+        if id + currentCardWins <= len(cards):
+            cards[id + currentCardWins].copies += card.copies
+        currentCardWins -= 1
+    id += 1
+    totalCards += card.copies
+
+print('Part 2: Total Cards = ', totalCards)
